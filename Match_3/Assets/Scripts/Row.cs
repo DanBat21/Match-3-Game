ccbb3e745402b9feb005b7da490409d8de496bdb
DanBat21 <DanBat03@gmail.com>
2022-11-17 00:54:25 +0300

    Commit_1
    Создал поле и сетку игры, реализовал основную игровую логику (перемещение игровых объектов, распознание от 3х одинаковых объектов рядом друг с другом и их удаление с игрового поля).

---
+++ Match_3/Assets/Scripts/Row.cs
@@ -0,0 +1,10 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public sealed class Row : MonoBehaviour
+{
+    public Tile[] tiles;
+    // Start is called before the first frame update
+   
+}
